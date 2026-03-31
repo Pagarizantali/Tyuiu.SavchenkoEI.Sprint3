d1c00352a7df970f99a713ba393f46f094f8df13
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 07:04:42 +0500

    Task1

---
+++ Tyuiu.SavchenkoEI.Sprint3.Task1.V17.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.SavchenkoEI.Sprint3.Task1.V17.Lib
+{
+    public class DataService : ISprint3Task1V17
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            int i = 0;
+            double res=1;
+            while (i<stopValue) 
+            {
+                res*=Math.Pow(2/(Math.Cos(value)),i+1);
+                i++;
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.SavchenkoEI.Sprint3.Task1.V17.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SavchenkoEI.Sprint3.Task1.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
