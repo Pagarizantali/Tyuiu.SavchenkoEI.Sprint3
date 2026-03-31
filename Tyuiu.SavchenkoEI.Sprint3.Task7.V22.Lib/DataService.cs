930d33900cbdf91e3d003a764d24928ca2cac8ce
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 07:56:21 +0500

    Task7

---
+++ Tyuiu.SavchenkoEI.Sprint3.Task7.V22.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.SavchenkoEI.Sprint3.Task7.V22.Lib
+{
+    public class DataService : ISprint3Task7V22
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray = new double[(stopValue - startValue) + 1];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round((Math.Sin(x) + (Math.Cos(2 * x) / 2) - 1.5 * x), 3);
+                valueArray[count++] = y;
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.SavchenkoEI.Sprint3.Task7.V22.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SavchenkoEI.Sprint3.Task7.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
