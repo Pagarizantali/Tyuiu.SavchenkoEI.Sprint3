ebfb545d347ff734a8d92b68aa3f86262329c213
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 06:49:33 +0500

    Add project files.

---
+++ Tyuiu.SavchenkoEI.Sprint3.Task0.V19.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.SavchenkoEI.Sprint3.Task0.V19.Lib
+{
+    public class DataService : ISprint3Task0V19
+    {
+        public double GetSumSeries(int startValue, int stopValue)
+        {
+            double sum = 0;
+            for (int i = startValue; i < stopValue; i++)
+            {
+                sum += Math.Pow(1 / (Math.Cos(i)+2), 2);
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.SavchenkoEI.Sprint3.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SavchenkoEI.Sprint3.Task0.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
