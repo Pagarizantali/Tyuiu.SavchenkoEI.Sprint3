90aba1e950946a2fd8d9591f1baeb768526741b4
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 07:12:15 +0500

    Task2

---
+++ Tyuiu.SavchenkoEI.Sprint3.Task2.V8.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.SavchenkoEI.Sprint3.Task2.V8.Lib
+{
+    public class DataService : ISprint3Task2V8
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            int i = 1;
+            double result = 1;
+            do
+            {
+                result *= 1 / (Math.Pow((Math.Cos(5) + 1), 2));
+                i++;
+            } while ((i < stopValue) && (startValue <= i));
+            return Math.Round(result, 3);
+        }
+    }
+}
