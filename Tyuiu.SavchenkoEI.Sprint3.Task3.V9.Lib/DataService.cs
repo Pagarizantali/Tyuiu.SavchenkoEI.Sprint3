a1830f3998c3abf262ca3101c49d4c12d4660f23
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 07:19:07 +0500

    Task3

---
+++ Tyuiu.SavchenkoEI.Sprint3.Task3.V9.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.SavchenkoEI.Sprint3.Task3.V9.Lib
+{
+    public class DataService : ISprint3Task3V9
+    {
+        public string ReplaceNumOnChar(string value, char item)
+        {
+            foreach (char itm in value)
+            {
+                if (Char.IsDigit(itm))
+                {
+                    value = value.Replace(itm, item);
+                }
+            }
+            return value;
+        }
+    }
+}
---
+++ Tyuiu.SavchenkoEI.Sprint3.Task3.V9.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SavchenkoEI.Sprint3.Task3.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
